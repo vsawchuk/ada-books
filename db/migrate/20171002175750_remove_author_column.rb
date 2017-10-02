74d8c24ec1d7833f9691c49ce598778f2c6a9f26
Kari Bancroft <kariabancroft@gmail.com>
2017-10-02 11:35:46 -0700

    Modify DB to support the one to many relationship between authors and books

---
+++ db/migrate/20171002175750_remove_author_column.rb
@@ -0,0 +1,5 @@
+class RemoveAuthorColumn < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :books, :author
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919175756) do
+ActiveRecord::Schema.define(version: 20171002180738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
     t.decimal "price"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
